0591b3e2c4de0545d5f95e4cc881642ae9431045
lumingrui <lomirus@qq.com>
2023-02-12 17:06:32 +0800

    refactor(main): add `run` function

--- src/main.rs
+++ src/main.rs
@@ -40,10 +40,14 @@ async fn main() {
         },
     };
 
+    run(args.root, host, args.port).await;
+}
+
+async fn run(root: String, host: String, port: u16) {
     let connections1 = Arc::new(Mutex::new(HashMap::new()));
     let connections2 = Arc::clone(&connections1);
-    let root_clone = args.root.clone();
+    let root_clone = root.clone();
 
     task::spawn(async move { watcher::watch(root_clone, &connections1).await });
-    server::serve(host, args.port, args.root, connections2).await;
+    server::serve(host, port, root, connections2).await;
 }
