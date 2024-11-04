1debc432a4467bc84ff2c2d1e76b99f688385c58
lomirus <lomirus@qq.com>
2024-11-04 17:09:30 +0800

    chore: add index_listing test cases

--- tests/test.rs
+++ tests/test.rs
@@ -96,3 +96,59 @@ async fn request() {
     let text = response.text().await.unwrap();
     assert!(!text.contains("<script>"));
 }
+
+#[tokio::test]
+async fn disable_index_listing() {
+    const HOST: &str = "127.0.0.1:8001";
+
+    let listener = listen(HOST, "./tests/empty_index").await.unwrap();
+    tokio::spawn(async {
+        listener
+            .start(Options {
+                hard_reload: true,
+                index_listing: false,
+            })
+            .await
+            .unwrap();
+    });
+
+    // Test requesting index.html
+    let response = reqwest::get(format!("http://{HOST}")).await.unwrap();
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+    let content_type = response.headers().get("content-type").unwrap();
+    assert_eq!(content_type, "text/html");
+
+    let text = response.text().await.unwrap().replace("\r\n", "\n");
+    assert!(text.starts_with("<!DOCTYPE html>"));
+    assert!(text.contains("<script>"));
+}
+
+#[tokio::test]
+async fn enable_index_listing() {
+    const HOST: &str = "127.0.0.1:8002";
+
+    let listener = listen(HOST, "./tests/empty_index").await.unwrap();
+    tokio::spawn(async {
+        listener
+            .start(Options {
+                hard_reload: true,
+                index_listing: true,
+            })
+            .await
+            .unwrap();
+    });
+
+    // Test requesting index.html
+    let response = reqwest::get(format!("http://{HOST}")).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let content_type = response.headers().get("content-type").unwrap();
+    assert_eq!(content_type, "text/html");
+
+    let text = response.text().await.unwrap().replace("\r\n", "\n");
+    assert!(text.ends_with(
+        "<body><ul><li><a href=\"not_index.html\">not_index.html</a></li></ul></body>\n</html>\n"
+    ));
+    assert!(text.contains("<script>"));
+}
